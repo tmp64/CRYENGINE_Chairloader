b51f577b18def0028e8a57be42ee3a06618318d1
Marcio Freitas <marcio@crytek.com>
2021-08-11 08:51:00 +0000

    !B (BREAK-588) (Build System) - Fix failing to download SDKS during solution generation. - Existence of extraction path is no longer condition for downloading/extracing SDKs package. - If an SDKs package is not found, it will be downloaded and extracted. - If an SDKs package exists but the destination folder does not, the package will be extracted.
    Copied from Perforce
     Change: 2275573

--- Tools/CMake/DownloadSDKs.cmake
+++ Tools/CMake/DownloadSDKs.cmake
@@ -1,4 +1,4 @@
-if (NOT EXISTS "${CRYENGINE_DIR}/Tools/branch_bootstrap/bootstrap.exe" AND NOT EXISTS "${SDK_DIR}")
+if (NOT EXISTS "${CRYENGINE_DIR}/Tools/branch_bootstrap/bootstrap.exe")
 	set(SDK_ARCHIVE   "CRYENGINE_v5.6.7_SDKs.zip")
 	set(GIT_TAG       "5.6.7")
 
@@ -8,10 +8,14 @@ if (NOT EXISTS "${CRYENGINE_DIR}/Tools/branch_bootstrap/bootstrap.exe" AND NOT E
 		message(STATUS "Downloading SDKs...")
 		file(DOWNLOAD "https://github.com/CRYTEK/CRYENGINE/releases/download/${GIT_TAG}/${SDK_ARCHIVE}"
 			"${CRYENGINE_DIR}/${SDK_ARCHIVE}" SHOW_PROGRESS)
+		set(FORCE_EXTRACT_SDKS 1)
 	endif()
 
-	message("Extracting ${SDK_ARCHIVE} to ${SDK_DIR}/...")
-	file(MAKE_DIRECTORY "${SDK_DIR}")
-	execute_process(COMMAND "${CMAKE_COMMAND}" "-E" "tar" "xzf" "${CRYENGINE_DIR}/${SDK_ARCHIVE}" WORKING_DIRECTORY "${SDK_DIR}")
-	message("Download and extraction of SDKs completed.")
+	if (NOT EXISTS "${SDK_DIR}" OR DEFINED FORCE_EXTRACT_SDKS)
+		file(MAKE_DIRECTORY "${SDK_DIR}")
+		message("Extracting ${SDK_ARCHIVE} to ${SDK_DIR}/...")
+		execute_process(COMMAND "${CMAKE_COMMAND}" "-E" "tar" "xzkmf" "${CRYENGINE_DIR}/${SDK_ARCHIVE}" WORKING_DIRECTORY "${SDK_DIR}")
+		message("Download and extraction of SDKs completed.")
+	endif()
 endif()
+
